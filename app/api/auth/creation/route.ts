6dad26bdba897c3c910e833dc9816e054d59d7b7
leopham <hongthaipro@gmail.com>
2024-07-10 14:36:00 +0700

    update db

---
+++ app/api/auth/creation/route.ts
@@ -0,0 +1,41 @@
+import prisma from "@/utils/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export async function GET() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || user === null || !user.id) {
+    return {
+      status: 401,
+      body: {
+        message: "Unauthorized",
+      },
+    };
+  }
+
+  const dbUser = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) {
+    await prisma.user.create({
+      data: {
+        id: user.id,
+        firstName: user.given_name ?? "",
+        lastName: user.family_name ?? "",
+        email: user.email ?? "",
+        profileImage:
+          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+      },
+    });
+  }
+
+  return Response.redirect(
+    process.env.NODE_ENV !== "production"
+      ? "http://localhost:3000"
+      : process.env.NEXT_PUBLIC_BASE_URL!
+  );
+}
